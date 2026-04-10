ec5fa3f74b325e46bfe7d96ab86100d93c9f5279
Labrock <vladimirpeshkov5@yandex.ru>
2026-04-10 15:17:32 +0300

    make image in admin

--- src/resources/views/admin/types/slides/item.blade.php
+++ src/resources/views/admin/types/slides/item.blade.php
@@ -1,9 +1,12 @@
-<div>
+<div class="px-indent-half w-[244px]">
     @if ($item->recordable->image)
         @php($image = $item->recordable->image)
         <a target="_blank" href="{{ route('thumb-img', ['template' => 'original', 'filename' => $image->file_name]) }}"
-           class="block rounded-base overflow-hidden">
-            Image
+           class="block rounded-base overflow-hidden min-h-[116px]">
+            <img
+                class="h-full object-cover object-center"
+                src="{{ route('thumb-img', ['template' => 'partner-slider-item', 'filename' => $image->file_name]) }}"
+                alt="">
         </a>
     @endif
     <div class="mt-indent-sm">{{ $item->title }}</div>
